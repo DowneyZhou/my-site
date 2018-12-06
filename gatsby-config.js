07120683fd1f749a9a0411d0a68c5f54f959c446
Downey Chow <downeychow@DowneydeMini.lan>
2018-12-06 23:13:20 +0800

    Style Update

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'DowneyChow',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div className="Header">
+      <div className="Logo">DC</div>
+      <div className="Nav">
+        <a>HOME</a>
+        <a>WORK</a>
+        <a>PHOTOGRAPHY</a>
+        <a>GET IN TOUCH</a>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
