71f4291650d30244f885466131ca199ad69b686f
Downey Chow <downeychow@DowneydeMini.lan>
2018-12-13 09:44:16 +0800

    Style updata

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,54 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <div className="Header">
-      <div className="Logo">DC</div>
-      <div className="Nav">
-        <Link to="page-2">HOME</Link>
-        <Link to="page-2">WORK</Link>
-        <Link to="page-2">PHOTOGRAPHY</Link>
-        <Link to="page-2">GET IN TOUCH</Link>
-      </div>
+<div>
+
+    <div className="Logo"></div>
+    <div className="Title">
+    <h1> 
+    Hello,I am DowneyChow — a produt designer from China 
+    who focuses on high quality vision and technology solutions to create a good experience for digital products.
+    </h1>
+  </div>
+
+   <div className="Nav">
+       <Link to="page-2">Creation</Link>
+       <Link to="page-2">Contact</Link>
+       <Link to="page-2">PHOTOGRAPHY</Link>
+    </div>
+
+  <div className="Article">
+    <div className="Content">
+      <h2>Santa Clara Santa ClaraSanta Clara</h2>
+      <div className="Cover"></div>
+      <p>I joined the design team at Opendoor in september 2016 to help them operate the first rebrand of the compagny. I helped redesign and code the V2 of the website, refresh the email infrastructure. Since the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.</p>
+      <Link className="Read-More">Read More</Link>
     </div>
   </div>
+
+  <div className="Article">
+    <div className="Content">
+      <h2>Santa Clara Santa ClaraSanta Clara</h2>
+      <div className="Cover"></div>
+      <p>I joined the design team at Opendoor in september 2016 to help them operate the first rebrand of the compagny. I helped redesign and code the V2 of the website, refresh the email infrastructure. Since the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.</p>
+      <Link className="Read-More">Read More</Link>
+    </div>
+  </div>
+
+  <div className="Article">
+    <div className="Content">
+      <h2>Santa Clara Santa ClaraSanta Clara</h2>
+      <div className="Cover"></div>
+      <p>I joined the design team at Opendoor in september 2016 to help them operate the first rebrand of the compagny. I helped redesign and code the V2 of the website, refresh the email infrastructure. Since the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.the launch of the brand 2.0 in january 2017.</p>
+      <Link className="Read-More">Read More</Link>
+    </div>
+  </div>
+
+  <footer>
+    <p>© 2018 — DowneyChow sometimes write code.</p>
+  </footer>
+  
+</div>
 )
 
 export default IndexPage
